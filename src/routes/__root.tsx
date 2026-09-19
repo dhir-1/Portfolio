33539f1d193d48474143babc5a4f3cf813c87c5e
dhir <dhiragrawal17@gmail.com>
2026-09-19 12:00:41 +0530

    feat: update favicon with transparent centered logo and add multi-res ICO

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -89,6 +89,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "icon", href: "/favicon.ico" },
+      { rel: "apple-touch-icon", href: "/favicon.png" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
